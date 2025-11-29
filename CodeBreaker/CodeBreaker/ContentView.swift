639517baa67f25a01a7997aeada97673ca6d689b
Miligen <lei@MateBook.local>
2025-11-29 17:32:39 +0800

    语法糖的简要记录Of "L1: Intro to Xcode and SwiftUI"

--- CodeBreaker/CodeBreaker/ContentView.swift
+++ CodeBreaker/CodeBreaker/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // VStack后的括号在采用默认值，且后面有一个函数拖尾时可以省略
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, CS193p!")
+            Circle() // 这里的circle的括号不可省略
         }
         .padding()
     }
